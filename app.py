90d0a704e1369e349a676aefd88954cfc15808b1
Matthew Drury <matthew.drury@galvanize.com>
2016-11-10 09:39:37 -0800

    Add submit page.

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/submit', methods=['GET'])
+def submit():
+    return render_template('submit.html')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0', debug=True)
